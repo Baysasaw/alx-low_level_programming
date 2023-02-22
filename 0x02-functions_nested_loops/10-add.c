bfa34f7163089664c3430228fee9636d82eff73b
baysasaw <yonas988252656@gmail.com>
2023-02-22 08:09:05 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,26 @@
+#include <stdio.c>
+/**
+ * add - a function that add two number
+ * a - int  parameter
+ * b - int  parameter
+ * Return: Always a + b
+ */
+int add(int a, int b);
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);
+	return (0);
+}
+int add(int, int)
+{
+	return (a + b);
+}
+
