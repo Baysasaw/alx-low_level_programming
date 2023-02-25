93a41bde8196f6595208075d94b2cf54ff3a03f1
baysasaw <yonas988252656@gmail.com>
2023-02-25 01:25:52 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
