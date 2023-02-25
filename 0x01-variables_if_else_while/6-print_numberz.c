94b857b0b840563e52e6ada2da8f02db25c5454a
baysasaw <yonas988252656@gmail.com>
2023-02-25 01:58:29 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		putchar(i + '0');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
