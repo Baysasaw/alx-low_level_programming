ed1423c7d9135b6c1501a47623f1d30533c42446
baysasaw <yonas988252656@gmail.com>
2023-03-07 00:05:20 -0800

    3. strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			if (accept[i] == s[j])
+			{
+				break;
+			}
+			else if (accept[i + 1] == '\0')
+				return (i + 1);
+		}
+	}
+	return (i + 1);
+}
