f2c13d563949c377f5ed9da283cc0b072a6efdca
baysasaw <yonas988252656@gmail.com>
2023-03-06 18:53:37 -0800

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "main.h"
+/**
+ * _strchr - Entry point
+ * @s: input string
+ * @c: input
+ * Return:  a pointer
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		if (*(s + i) == c)
+			return (s + i);
+	}
+	return (0);
+}
