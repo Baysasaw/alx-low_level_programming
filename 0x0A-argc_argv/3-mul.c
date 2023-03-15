b095b42e99afe183e92fdab983bdb92f930fbeac
baysasaw <yonas988252656@gmail.com>
2023-03-15 17:30:25 +0300

    Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,8 +12,8 @@ int main(int argc, char *argv[])
 
 	if (argc == 3)
 	{
-		mult = atoi(argv[1]) + atoi(argv[2]);
-		printf("%d", mult);
+		mult = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mult);
 	}
 	else
 	{
