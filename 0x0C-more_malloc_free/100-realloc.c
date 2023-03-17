15a26abc0ec52b1a4bc200dcd99cf6adee39b500
baysasaw <yonas988252656@gmail.com>
2023-03-17 15:33:40 +0000

    _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,11 +17,18 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	old_ptr = ptr;
 	ptr1 = malloc(new_size);
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+			ptr1[i] = old_ptr[i];
+	}
+
 	if (new_size > old_size)
 	{
 		for (i = 0; i < old_size; i++)
 			ptr1[i] = old_ptr[i];
 	}
+
 	if (new_size == old_size)
 		return (ptr);
 	if (ptr == NULL)
