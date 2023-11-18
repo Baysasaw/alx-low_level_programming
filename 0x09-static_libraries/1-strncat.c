9e97ea136689003489ff0faae76c619362a1d820
baysasaw <yonas988252656@gmail.com>
2023-11-18 05:52:06 +0300

    0x09. C - Static libraries

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,28 +1,27 @@
 #include "main.h"
 /**
- * _strncat - a function that concatenates two strings.
- * @dest: a string
- * @src: a string
- * @n: it will use at most n bytes from src
- * Return: dest
+ * _strncat - concatenates two strings,
+ * @dest: destination.
+ * @src: source.
+ * @n: amount of bytes used from src.
+ * Return: the pointer to dest.
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0;
-	int j = 0;
+	int count = 0, count2 = 0;
 
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
 
-	while (dest[i] != '\0')
+	while (count2 < n)
 	{
-		i++;
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
 	}
-	while (j < n && src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
 	return (dest);
-
 }
--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,19 +1,18 @@
 #include "main.h"
-#include "main.h"
 /**
- * _strchr - Entry point
- * @s: input string
- * @c: input
- * Return:  a pointer
+ * _strchr - locates a character in a string,
+ * @s: string.
+ * @c: character.
+ * Return: the pointer to the first occurrence of the character c.
  */
 char *_strchr(char *s, char c)
 {
-	int i;
+	unsigned int i = 0;
 
-		for (i = 0; *(s + i) >= '\0'; i++)
-	{
+	for (; *(s + i) != '\0'; i++)
 		if (*(s + i) == c)
-			return ((s + i));
-	}
-	return (0);
+			return (s + i);
+	if (*(s + i) == c)
+		return (s + i);
+	return ('\0');
 }
